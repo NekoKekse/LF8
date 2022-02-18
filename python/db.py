42ef36ea587d78876f19c2f9b5b5c0413cc2b11c
NekoKekse <59479818+NekoKekse@users.noreply.github.com>
2022-02-18 12:10:44 +0100

    Create db.py

---
+++ python/db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+dbfile = "database.db"
+
+con = sqlite3.connect(dbfile)
+cur = con.cursor()
+
+# Datenbank Table erstellen
+
+cur.execute('''CREATE TABLE IF NOT EXISTS HW (test text)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS SW (test text)''')
+
+# Daten in die Datenbank einfügen
+
+def add (table, value):
+    cur.execute("INSERT INTO table (test) VALUE (?)")
