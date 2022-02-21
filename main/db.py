f2aecaffa5ef3f6fe696618f320a6cf2aa6217f5
NekoKekse <nekokekse@gmx.de>
2022-02-21 12:20:16 +0100

    Base Für SQLite V2

--- main/db.py
+++ main/db.py
@@ -2,8 +2,6 @@ import sqlite3
 
 dbfile = "database.db"
 
-
-# Daten in die Datenbank einfügen
 con = sqlite3.connect(dbfile)
 cur = con.cursor()
 class DB:
@@ -11,10 +9,10 @@ class DB:
         cur.execute('''CREATE TABLE IF NOT EXISTS HW (test text)''')
         cur.execute('''CREATE TABLE IF NOT EXISTS SW (test text)''')
         
-    def add(table):
-        fTable = table.higher()
-
-        cur.execute("INSERT INTO " + fTable + "")
+    def add(table_name, data):
+        fTable_name = table_name.higher()
+        fData = data.replace('"', '')
+        cur.execute("INSERT INTO " + fTable_name + " VALUES " + "(" + str(fData) + ")" )
 
 con.commit()
 con.close()
