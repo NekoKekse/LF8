29ebb05dda8f5c7280ecef3b8c1dcfd9dc5b9372
NekoKekse <59479818+NekoKekse@users.noreply.github.com>
2022-02-23 10:17:10 +0100

    V3

--- main/db.py
+++ main/db.py
@@ -4,15 +4,41 @@ dbFile = "database.db"
 
 con = sqlite3.connect(dbFile)
 cur = con.cursor()
+
 class DB:
     def build():
-        cur.execute('''CREATE TABLE IF NOT EXISTS HW (test text)''')
-        cur.execute('''CREATE TABLE IF NOT EXISTS SW (test text)''')
-        
-    def add(table_name, data):
-        fTable_name = table_name.higher()
-        fData = data.replace('"', '')
-        cur.execute("INSERT INTO " + fTable_name + " VALUES " + "(" + str(fData) + ")" )
+        sqlCreateTable = '''CREATE TABLE IF NOT EXISTS hw (
+            Datum date,
+            Zeit time,
+            CPU_Auslastung int,
+            GPU_Auslastung int,
+            RAM_Auslastung int,
+            NetzwerkAuslatung int,
+            CPU_Temperatur double(10, 2),
+            GPU_Temperatur double(10, 2),
+            RAM_Temperatur double(10, 2),
+            FestplattenSpeicher_Maximal double(10, 2),
+            FestplattenSpeicher_Frei double(10, 2);'''
 
-con.commit()
+        cur.execute(sqlCreateTable)
+
+        con.commit()
+
+    def add(date, time, acpu, agpu, aram, anetz, tcpu, tgpu, tram, max_disk_space, used_disk_space):
+        sqlInsert = "INSERT INTO hw (Datum, Zeit, CPU_Auslastung, GPU Auslastung, RAM_Auslastung, NetzwerkAuslastung, CPU_Temperatur, GPU_Temperatur, RAM_Temperatur, FestplattenSpeicher_Maximal, FestplattenSpeicher_Frei) \
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+
+        sqlInsertData = (date, time, acpu, agpu, aram, anetz, tcpu, tgpu, tram, max_disk_space, used_disk_space)
+
+        cur.execute(sqlInsert, sqlInsertData)
+
+        con.commit()
+
+    def select(date):
+        sqlSelect = "SELECT * FROM hw WHERE Datum = ?;"
+
+        sqlSelectData = (date)
+
+        cur.execute(sqlSelect, sqlSelectData)
+
 con.close()
