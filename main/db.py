b20b204b44d53d36d51bfc64396ee8a6d3986042
NekoKekse <59479818+NekoKekse@users.noreply.github.com>
2022-02-24 10:55:21 +0100

    Update db.py

--- main/db.py
+++ main/db.py
@@ -13,19 +13,19 @@ class DB:
             CPU_Auslastung int,
             GPU_Auslastung int,
             RAM_Auslastung int,
-            NetzwerkAuslatung int,
+            NetzwerkAuslastung int,
             CPU_Temperatur double(10, 2),
             GPU_Temperatur double(10, 2),
             RAM_Temperatur double(10, 2),
             FestplattenSpeicher_Maximal double(10, 2),
-            FestplattenSpeicher_Frei double(10, 2);'''
+            FestplattenSpeicher_Frei double(10, 2));'''
 
         cur.execute(sqlCreateTable)
 
         con.commit()
 
     def add(date, time, acpu, agpu, aram, anetz, tcpu, tgpu, tram, max_disk_space, used_disk_space):
-        sqlInsert = "INSERT INTO hw (Datum, Zeit, CPU_Auslastung, GPU Auslastung, RAM_Auslastung, NetzwerkAuslastung, CPU_Temperatur, GPU_Temperatur, RAM_Temperatur, FestplattenSpeicher_Maximal, FestplattenSpeicher_Frei) \
+        sqlInsert = "INSERT INTO hw (Datum, Zeit, CPU_Auslastung, GPU_Auslastung, RAM_Auslastung, NetzwerkAuslastung, CPU_Temperatur, GPU_Temperatur, RAM_Temperatur, FestplattenSpeicher_Maximal, FestplattenSpeicher_Frei) \
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
 
         sqlInsertData = (date, time, acpu, agpu, aram, anetz, tcpu, tgpu, tram, max_disk_space, used_disk_space)
@@ -35,10 +35,9 @@ class DB:
         con.commit()
 
     def select(date):
-        sqlSelect = "SELECT * FROM hw WHERE Datum = ?;"
+        cur.execute("SELECT * FROM hw WHERE Datum = ?", (date,))
 
-        sqlSelectData = (date)
+        hwdata = cur.fetchall()
 
-        cur.execute(sqlSelect, sqlSelectData)
-
-con.close()
+        for data in hwdata:
+            print(data)
