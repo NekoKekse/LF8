af50f3cfe79b97a01cbae03c912f9ca244f40e91
Justin-py <selyjust1@gmail.com>
2022-02-24 12:28:42 +0000

    Change for DBtest

--- main/Main.py
+++ main/Main.py
@@ -1,5 +1,15 @@
-from lib2to3.pgen2.token import EQUAL
 import sys
+from sqltest import dbtest
+from datetime import datetime
+
+class mainfunktions():
+    def date(self):
+        x = datetime.now()
+        return x.strftime("%Y-%m-%d")
+
+    def time(self):
+        x = datetime.now()
+        return x.strftime("%H:%M:%S")
 
 if __name__ == '__main__':
     if len(sys.argv)==1:
@@ -8,8 +18,9 @@ if __name__ == '__main__':
         print(sys.argv[1])
         if sys.argv[1] == '-L':
             print('Log')
-        elif sys.argv[1]=='-D' and len(sys.argv)==1:
-            print('debug')
+        elif sys.argv[1]=='-D':
+            dbtest.add(mainfunktions.date, mainfunktions.time)
+            print(dbtest.select(mainfunktions.date))
         else:
             print('''Use: 
 -D      Debug Mode
--- main/sqltest.py
+++ main/sqltest.py
@@ -1,27 +1,13 @@
-import sqlite3
+from time import time
 from db import DB
 
-from datetime import datetime
+filename = 'test.db'
 
-DB.build()
+class dbtest():
 
-x = datetime.now()
-date = x.strftime("%Y-%m-%d")
-time = x.strftime("%H:%M:%S")
-
-
-print(date)
-print(time)
-
-DB.add(str(date), str(time), 1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-con = sqlite3.connect('database.db')
-cur = con.cursor()
-
-sqlSelect = "SELECT * FROM hw;"
-
-cur.execute(sqlSelect)
-
-for row in cur:
-    print(row[0])
-    print(str(row[1])) 
+    def add(date, time):
+        DB.build(filename)
+        DB.add(filename, str(date), str(time), 1, 2, 3, 4, 5, 6)
+    
+    def select(date):
+        return DB.select(filename, self.date)
