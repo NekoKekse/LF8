58f973a0a2ed246abe338d30a6b77d87591544aa
Bagun <94955414+DerBagun@users.noreply.github.com>
2022-03-01 11:45:44 +0100

    Update unittests.py

--- main/tests/unittests.py
+++ main/tests/unittests.py
@@ -1,19 +1,19 @@
-#bruh kein bock
 import unittest
-import 
+import hardware_monitoring as monitor
+
+print(monitor.used_disk_percent())
+
+class HardwareUnittests(unittest.TestCase):
+
+    def test_ram_prct(self):
+        self.assertLess(monitor.get_ram_usage_prct(), 100.0)
+
+    def test_cpu_prct(self):
+        self.assertLess(monitor.used_cpu_percent(), 100.0)
+
+    def test_cpu_temp(self):
+        self.assertLess(monitor.tem_cpu(), 150.0)
 
 
-class TestBeispieleTestCase(unittest.TestCase):      
-                                                       
-  def test_rundung(self):                          
-    self.assertAlmostEqual(cpu_temp, cpu_kurz, 2)
-                                                       
-  def test_                        
-        
-        
-        
-        
-if __name__ == '__main__':                           
-  unittest.main(verbosity=0)                       
-  
-  
+if __name__ == '__main__':
+    unittest.main()
