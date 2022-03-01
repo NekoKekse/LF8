d022981c09c3e57d15f59793f64959d365ae7f84
Bagun <94955414+DerBagun@users.noreply.github.com>
2022-03-01 10:16:42 +0100

    Update HardwareMonitoring_Changes.py

--- main/HardwareMonitoring_Changes.py
+++ main/HardwareMonitoring_Changes.py
@@ -1,92 +1,86 @@
-# repo Import
+#!/usr/bin/env python3
+'''Hardware Data Part'''
 import psutil
-import unittest
+from platform import system
 import re
 import urllib.request
 
 
-# Temperatur Check
-class monitor:
+from psutil import cpu_times_percent
 
-    def temp():
-        # Temperatur Check
-        #if platform.system == 'Windows':
-            import wmi
-            current_temp = wmi.WMI(
-                namespace="root\\wmi")  # Die Temperatur funktioniert nicht wirklich es wird immer der selbe wert angezeigt...¯\_(ツ)_/¯ kann an dem Laptop liegen oder generelles Problem
-            cpu_temp = (current_temp.MSAcpi_ThermalZoneTemperature()[0].CurrentTemperature / 10.0) - 273.15
-            return round(cpu_temp, 2)
 
-    def user():
-        # User Check
-        user_list = psutil.users()
-        user_str = "".join(map(str, user_list))
-        username = re.search('=(.+?),', user_str).group(1)
-        return username[1:-1] + ' ist eingeloggt'
+#Basics
+def tem_cpu():
+    '''Return CPU temp'''
+    import platform
+    if platform.system()=='Windows':
+        import wmi
+        current_temp = wmi.WMI(namespace="root\\wmi") #Die Temperatur funktioniert nicht wirklich es wird immer der selbe wert angezeigt...¯\_(ツ)_/¯ kann an dem Laptop liegen oder generelles Problem
+        cpu_temp = (current_temp.MSAcpi_ThermalZoneTemperature()[0].CurrentTemperature / 10.0)-273.15
+        return round(cpu_temp, 2)
+        # Überarbeiten: https://psutil.readthedocs.io/en/latest/index.html?highlight=sensors_temperatures#sensors
+    elif platform.system()=='Linux':
+        return psutil.sensors_temperatures()
 
-    def disk_prct():
-        # Variablen für Speicherplatz Abfrage
-        disk_name = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
-        fehler = 0
-        for i in disk_name:
-            try:
-                print(i + ":/ benutzt" , psutil.disk_usage(i + ':/').percent , "% Festplattenspeicher")
-            except OSError as err:
-                # print(i +':/ ist nicht angeschlossen')
-                fehler = fehler + 1
+def used_cpu_percent():
+    '''Return CPU usage in percet'''
+    return psutil.cpu_percent(5)
 
-    def disk_GB():
-        disk_name = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
-        fehler = 0
-        for i in disk_name:
-            try:
-                print(i + ":/ hat " + str(round(((((psutil.disk_usage(i + ':/').total) /1024 ) /1024 ) /1024 ),2)),'GB')
-            except OSError as err:
-                # print(i +':/ ist nicht angeschlossen')
-                fehler = fehler + 1
-
-    def disk_GB_free():
-        disk_name = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
-        fehler = 0
-        for i in disk_name:
-            try:
-                print(i + ":/ hat " + str(round(((((psutil.disk_usage(i + ':/').free) /1024 ) /1024 ) /1024 ),2)),'GB frei')
-            except OSError as err:
-                # print(i +':/ ist nicht angeschlossen')
-                fehler = fehler + 1
-
-    def get_cpu_usage():
-        # CPU Auslastung Abfrage
-        return psutil.cpu_percent(5)
-
-    def get_ram_usage():
-        # RAM Auslastung Abfrage in MB
-        return int(((psutil.virtual_memory().total - psutil.virtual_memory().available) /1024) /1024)
-
-    def get_ram_usage_prct():
-        # RAM Auslastung Abfrage in Prozent
-        return psutil.virtual_memory().percent
-
-    def get_ram_total():
-        return int((((psutil.virtual_memory().total) /1024 ) /1024 ) /1024 )
-
-    def connect(google='http://google.com'):
-        # Netzwerk Test (ist Google erreichbar?)
+def used_disk_percent():
+    # Variablen für Speicherplatz Abfrage
+    disk_name = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+    fehler = 0
+    for i in disk_name:
         try:
-            urllib.request.urlopen(google)
-            return True
-        except:
-            return False
+            file_system = (i + ":/ benutzt", psutil.disk_usage(i + ':/').percent, "% Festplattenspeicher")
+        except OSError as err:
+            # print(i +':/ ist nicht angeschlossen')
+            fehler = fehler + 1
+    return file_system
+
+def free_disk_gb():
+    '''Returns free space on disk'''
+    disk_name = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+    file_system = ""
+    fehler = 0
+    for i in disk_name:
+        try:
+            file_system = (i,str(round(((((psutil.disk_usage(i + ':/').free) /1024 ) /1024 ) /1024 ),2)))
+        except OSError as err:
+            # print(i +':/ ist nicht angeschlossen')
+            fehler = fehler + 1
+    return file_system
+
+def get_ram_usage():
+    # RAM Auslastung Abfrage in MB
+    return int(((psutil.virtual_memory().total - psutil.virtual_memory().available) /1024) /1024)
+
+def get_ram_usage_prct():
+    # RAM Auslastung Abfrage in Prozent
+    return psutil.virtual_memory().percent
 
 
-#print(psutil.users())
+def user():
+    '''Retuns registered user/s'''
+    user_list=psutil.users()
+    user_str="".join(map(str,user_list))
+    username = re.search('=(.+?),', user_str).group(1)
+    return username[1:-1]
+
+def connection(url='http://google.com'):
+    '''Ping'''
+    try:
+        urllib.request.urlopen(url)
+        return True
+    except ValueError:
+        return False
 
 
-# monitor.temp
-# monitor.user
-# monitor.
 
-# Ergebnisse der Tests werden ausgegeben
+if __name__=='__main__':
+    print(str(tem_cpu()) + ' # ' + str(used_cpu_percent()) + ' # ' + str(used_disk_percent()) + ' # ' + str(free_disk_gb()) + ' # ' + str(user()) + ' # ' + str(connection()))
+
+#Ergebnisse der Tests werden ausgegeben
 
 # print(cpu_kurz,'°C warm')
 # print('Die CPU Auslastung beträgt: {}%'.format(get_cpu_usage()))
@@ -95,12 +89,26 @@ class monitor:
 # print('Eine Internetverbindung ist vorhanden' if connect() else 'Kein Internet!')
 
 
-# Unittests
-class TestBeispieleTestCase(unittest.TestCase):
-
-    def test_rundung(self):
-        self.assertAlmostEqual(cpu_temp, cpu_kurz, 2)
 
 
-if __name__ == '__main__':
-    unittest.main(verbosity=0)
+
+
+
+
+
+
+
+
+
+
+
+
+#Unittests
+# class TestBeispieleTestCase(unittest.TestCase):
+
+#     def test_rundung(self):
+#         self.assertAlmostEqual(cpu_temp, cpu_kurz, 2)
+
+
+# if __name__ == '__main__':
+#     unittest.main(verbosity=0)
