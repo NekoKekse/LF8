2656f97c671b2487b4fc83b31c4e268b1aec9bdf
Bagun <94955414+DerBagun@users.noreply.github.com>
2022-03-01 11:15:38 +0100

    Update and rename unittests to unittests.py

---
+++ main/tests/unittests.py
@@ -0,0 +1,19 @@
+#bruh kein bock
+import unittest
+import 
+
+
+class TestBeispieleTestCase(unittest.TestCase):      
+                                                       
+  def test_rundung(self):                          
+    self.assertAlmostEqual(cpu_temp, cpu_kurz, 2)
+                                                       
+  def test_                        
+        
+        
+        
+        
+if __name__ == '__main__':                           
+  unittest.main(verbosity=0)                       
+  
+  
